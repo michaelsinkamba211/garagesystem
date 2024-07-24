c6c8bcbe2ce53c527596448343785674093e9a3e
Webby.dev <webbychileshe27@gmail.com>
2024-07-24 08:53:21 +0200

    Update StaffUpdate.php

--- StaffUpdate.php
+++ StaffUpdate.php
@@ -8,7 +8,7 @@ $query = "SELECT * FROM employee WHERE Employee_id = '$id'";
 $result = mysqli_query($db, $query);
 $row = mysqli_fetch_assoc($result);
 
-if(isset($_POST['updatebtn']))
+if(isset($_POST['update_btn']))
 {
     $newName = $_POST['name'];
     $newEmail = $_POST['email'];
@@ -22,35 +22,44 @@ if(isset($_POST['updatebtn']))
 ?>
 <div class="container-fluid d-flex justify-content-center">
 
-<div class="card shadow mb-4 w-50">
-  <div class="card-header h-800 py-4">
+<div class="card shadow  w-100">
+  <div class="card-header py-0">
   <h6 class="ml-2 font-weight-bold row">
   <a class="link l-20" href="Staff.php">
             <button type="button" class="btn btn-danger pull-left mt-2 mr-2">BACK</button></a>
          <hr><br>
     </h6>
     <form method="POST" class=" justify-content-center">
-            <div class="form-group w-50">
-                <label class="text-dark text-weight-bold">Employee Name</label>
-                <input type="text" name="name" value="<?php echo $row['Employee_Name'];?>" class="form-control" required >
+
+    
+<div class="container w-50">
+  <form method="POST" action="update.php" enctype="multipart/form-data">
+    <div class="mb-3">
+            <div>
+            <label for="first-name" class="form-label">Full Name</label>
+            <input type="text" name="name" value="<?php echo $row['Employee_Name'];?>" class="form-control" required >
             </div>
-            <div class="form-group w-50">
-                <label class="text-dark text-weight-bold">Email</label>
-                <input type="text" name="email" value="<?php echo $row['Email'];?>" class="form-control" required >
+            <div>
+            <label for="last-name" class="form-label">Email</label>
+            <input type="text" name="email" value="<?php echo $row['Email'];?>" class="form-control" required >
             </div>
-            <div class="form-group w-50">
-                <label class="text-dark text-weight-bold">Password</label>
-                <input type="password" name="password" value="<?php echo $row['Password'];?>" class="form-control" required >
+            <div>
+            <label for="phone" class="form-label">Password</label>
+            <input type="password" name="password" value="<?php echo $row['Password'];?>" class="form-control" required >
             </div>
-            <div class="form-group w-50">
-                <label class="text-dark text-weight-bold">Outlet Name</label>
-                <input type="text" name="outlet" value="<?php echo $row['Outlet_Name'];?>" class="form-control" required >
+            <div>
+            <label for="position" class="form-label">Outlet</label>
+            <input type="text" name="outlet" value="<?php echo $row['Outlet_Name'];?>" class="form-control" required >
             </div>
-            <div class="form-group w-50">
-                <label class="text-dark text-weight-bold">Position</label>
-                <input type="text" name="position" value="<?php echo $row['Position'];?>" class="form-control" required >
+            <div class="mb-3">
+            <label for="garage-number" class="form-label">Position</label>
+            <input type="text" name="position" value="<?php echo $row['Position'];?>" class="form-control" required >
             </div>
-            <button type="submit" name="updatebtn" class="btn btn-primary mr-3">Update</button>
+            <div class="modal-footer">
+            <button type="submit" name="update_btn" class="btn btn-primary">Update</button>
+            </div>
+        </form>
+        </div>
       </form>
       </div>
 </div>
